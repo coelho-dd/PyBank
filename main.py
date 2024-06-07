8e6113ebcdb2c11fe3094f9a2038101f571f6cbb
Davi Coelho <davicoelho.cont@gmail.com>
2024-06-07 19:48:33 -0300

    Version 1.0 done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # checking account = conta corrente e savings account = conta poupança
 import random
+import sys
 from account import Account
 
 def main():
@@ -18,6 +19,7 @@ def main():
             print("I'm sorry... I could not understand you. Please try again.")
 
 def login():
+    """ Creates an instance of the Account class that represents the account of an user """
     owner = input("Your name: ")
     number = random.randint(100, 999)
     credit = input("The amount of money you wish to deposit on your account[type 0 for none]: ")
@@ -31,7 +33,32 @@ def login():
     return account
 
 def pybank(obj):
-    pass
+    """ Contains the logic of the program that resembles an atm machine """
+    print("This is the screen where you can perform actions on your account, such as:")
+    print("Deposit and withdraw money, visualize your bank statement and so on...")
+    print("\nSelect one of the options below to execute it:")
+    print("1[withdraw], 2[deposit], 3[check your amount], 4[check bank statement] and 5[to quit the app]")
+
+    while True:
+        u_input = input(">>> ")
+        u_input = u_input.lower()
+
+        if u_input == "1":
+            w_value = input("What would be the amount that you'd like to withdraw: ")
+            w_value = int(w_value)
+            obj.withdraw(w_value)
+        elif u_input == "2":
+            d_value = input("What would be the amount that you'd like to deposit: ")
+            obj.deposit(d_value)
+        elif u_input == "3":
+            obj.check_amount()
+        elif u_input == "4":
+            obj.bank_statement()
+        elif u_input == "5":
+            print("Logging you out...")
+            sys.exit()
+        else:
+            print("I'm sorry... I could not understand you. Please try again.")
 
 # Se eu estou executando o interpretador a partir desse arquivo, então execute a função main
 if __name__ == "__main__":
